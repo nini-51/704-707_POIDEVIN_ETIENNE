ee93121844cdf232aff2bf0075575e7e230ba81d
toto <toto0example.com>
2023-01-27 13:17:09 +0100

    pages web

--- datacenter/web/app/main.py
+++ datacenter/web/app/main.py
@@ -4,8 +4,8 @@ from werkzeug.exceptions import abort
 
 from app.db import init_app, get_db
 
-api = Flask(__name__)
-api.config.from_mapping(
+app = Flask(__name__)
+app.config.from_mapping(
     # a default secret that should be overridden by instance config
     SECRET_KEY = "dev",
     # path of the database
@@ -14,37 +14,35 @@ api.config.from_mapping(
 # CORS(app, resources={r"/*": {"origins": "*"}})
 
 # register the database commands
-init_app(api)
+init_app(app)
 
 
-@api.route('/')
+@app.route('/')
 def update_package(package_id):
     """
-    Update a package
+    Home page; redirect to find package page
     """
-    return "soon"
+    return redirect(url_for('/find_my_package'))
 
-@api.route('/suivre_mon_colis')
+@app.route('/find_my_package')
 def update_package(package_id):
     """
-
+    Page to find a package with exact package id
     """
-    if(arg)
     db = get_db()
+    search = request.args.get('search')
 
-    cur = con.cursor()
-    cur.execute("SELECT * FROM packages WHERE package_id = ")
+    if search:
+        try:
+            package = db.execute("SELECT * FROM packages WHERE package_id = ?", (search.upper())).fetchone()
+        except db.DatabaseError as error:
+            abord(400, error)
 
-    packages = cur.fetchall();
-    return "soon"
+    return render_template('search.html', package = package)
 
-@app.route('/<int:package_id>/')
-def package(package_id):
-    package = package.query.get_or_404(package_id)
-    return render_template('package.html', package=package)
 
-@api.route('/liste')
-def liste(package_id):
+@app.route('/list')
+def list(package_id):
     """
     Update a package
     """
@@ -52,101 +50,6 @@ def liste(package_id):
 
     cur = con.cursor()
     cur.execute("SELECT * FROM packages")
-
     packages = cur.fetchall();
-    return render_template("liste.html", packages = packages)
 
-
-
-@api.route('/suivre_mon_colis/<string:package_id>')
-def update_package(package_id):
-    """
-    Update a package
-    """
-    return "soon"
-
-
-
-# # compose_flask/app.py
-# from flask import Flask, request, jsonify
-# import json
-# import sqlite3
-# # from flask_cors import CORS
-#
-#
-# web = Flask(__name__)
-# # CORS(app, resources={r"/*": {"origins": "*"}})
-#
-#
-#
-# def connect_db_connection():
-#     conn = sqlite3.connect('database.db')
-#     return conn
-#
-# def get_objects():
-#     users = []
-#     try:
-#         conn = connect_to_db()
-#         conn.row_factory = sqlite3.Row
-#         cur = conn.cursor()
-#         cur.execute("SELECT * FROM objects")
-#         rows = cur.fetchall()
-#
-#         # convert row objects to dictionary
-#         for i in rows:
-#             object = {}
-#             object["object_id"] = i["object_id"]
-#             object["status"] = i["status"]
-#             object["warehouse_id"] = i["warehouse_id"]
-#             object["deliver_id"] = i["deliver_id"]
-#             object["time"] = i["time"]
-#             object["gps"] = i["gps"]
-#             objects.append(object)
-#
-#     except:
-#         objects = []
-#
-#     return objects
-#
-#
-# def get_object_by_id(object_id):
-#     object = {}
-#     try:
-#         conn = connect_to_db()
-#         conn.row_factory = sqlite3.Row
-#         cur = conn.cursor()
-#         cur.execute("SELECT * FROM objects WHERE object_id = ?",
-#                        (object_id,))
-#         row = cur.fetchone()
-#
-#         # convert row object to dictionary
-#         object["object_id"] = row["object_id"]
-#         object["status"] = row["status"]
-#         object["warehouse_id"] = row["warehouse_id"]
-#         object["deliver_id"] = row["deliver_id"]
-#         object["time"] = row["time"]
-#         object["gps"] = row["gps"]
-#     except:
-#         object = {}
-#
-#     return object
-#
-#
-# @app.route('/', methods=['GET'])
-# def home():
-#     return '''<h1>VLib - Online Library</h1><p>A flask api implementation for book information.   </p>'''
-#
-#
-# @app.route('/api/objects', methods=['GET'])
-# def api_get_objects():
-#     return jsonify(get_objects())
-#
-# @app.route('/api/objects/<object_id>', methods=['GET'])
-# def api_get_object(object_id):
-#     return jsonify(get_object_by_id(object_id))
-#
-#
-# if __name__ == "__main__":
-#     #app.debug = True
-#     #app.run(debug=True)
-#     web.run() #run app
+    return render_template("list.html", packages = packages)
